6c12029310cce771b468f4d07586a7ec5ac1886c
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2026-01-17 15:16:06 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+//  Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+    
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return (x == reversed) || (x == reversed / 10);
+    }
+}
