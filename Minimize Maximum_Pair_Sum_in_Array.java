ab0b404463276d31e189eea8eae33a6a7acacedc
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2026-01-25 02:46:54 +0530

    Minimize Maximum Pair Sum in Array

---
+++ Minimize Maximum_Pair_Sum_in_Array.java
@@ -0,0 +1,24 @@
+// The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+// For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+// Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+// Each element of nums is in exactly one pair, and
+// The maximum pair sum is minimized.
+// Return the minimized maximum pair sum after optimally pairing up the elements.
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        int rPointer = nums.length -1;
+        int lPointer = 0;
+        while (lPointer <  rPointer){
+            if(sum < nums[lPointer] + nums[rPointer]){
+                sum = nums[lPointer] + nums[rPointer];
+            }
+            rPointer--; lPointer++;
+        }
+        return sum;
+    }
+}
