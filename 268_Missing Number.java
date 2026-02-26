da4700ce65bf0a8aee085fbdade7a509606e300f
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2026-02-26 23:27:44 +0530

    Missing Number
    Implement a solution to find the missing number in an array.

---
+++ 268_Missing Number.java
@@ -0,0 +1,11 @@
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+        return xor;
+    }
+}
