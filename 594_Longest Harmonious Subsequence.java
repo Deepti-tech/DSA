638c312f4b56f95d73f47bceffb08f84c394b061
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-05-11 00:22:00 +0530

    Longest Harmonious Subsequence

---
+++ 594_Longest Harmonious Subsequence.java
@@ -0,0 +1,17 @@
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, maxLen = 0;
+        for (int right = 0; right < nums.length; right++) {
+            while (nums[right] - nums[left] > 1) {
+                left++;  
+            }
+            if (nums[right] - nums[left] == 1) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+    }
+}
