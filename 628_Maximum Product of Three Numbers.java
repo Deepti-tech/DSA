3993c45ec74840eb53c6e3ae8d728db33778bbd4
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-06-21 19:57:11 +0530

    Update 628_Maximum Product of Three Numbers.java

--- 628_Maximum Product of Three Numbers.java
+++ 628_Maximum Product of Three Numbers.java
@@ -1,10 +1,13 @@
 class Solution {
     public int maximumProduct(int[] nums) {
         int n = nums.length;
-        int max1 = max2 = max3 = Integer.MIN_VALUE;
-        int min1 = min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE; 
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
         if(n>=3){
-            for(num : nums){
+            for(int num : nums){
                 if(num > max1){
                     max3 = max2;
                     max2 = max1;
@@ -23,6 +26,10 @@ class Solution {
                     min2 = num;
                 }
             }
+            int prod1 = max1 * max2 * max3;
+            int prod2 = min1 * min2 * max1;
+
+            return Math.max(prod1, prod2);
         }
         return 0;
     }
