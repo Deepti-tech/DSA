7c16fd92397de1e5ed6551c171658341eb38f105
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-08-02 16:30:04 +0530

    Create 877_Stone Game.java

---
+++ 877_Stone Game.java
@@ -0,0 +1,7 @@
+// Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins.
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
