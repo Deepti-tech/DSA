63dff71e7c80b4b5e5f36b526b5dbba823cf3a19
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-09-09 08:47:47 +0530

    Refactor countCommas method for simplicity

--- 3870_Count Commas in Range.java
+++ 3870_Count Commas in Range.java
@@ -1,19 +1,25 @@
 class Solution {
-    public int countCommas(int n) {
-        int len=0, num = n;
-        while (num>0){
-            num = num/10;
-            len++;
-        }
-        if(len < 4){
-            return 0;
-        }
-
-        int nOfCommas = (int) Math.floor(len/3);
-        nOfCommas = len%3 == 0 ? nOfCommas-1 : nOfCommas;
-
-        int count = n-1000;
-        
-        return nOfCommas * (count+1);        
+    public int countCommas(int n) {        
+        return Math.max(n-999, 0);        
     }
 }
+
+// class Solution {
+//     public int countCommas(int n) {
+//         int len=0, num = n;
+//         while (num>0){
+//             num = num/10;
+//             len++;
+//         }
+//         if(len < 4){
+//             return 0;
+//         }
+
+//         int nOfCommas = (int) Math.floor(len/3);
+//         nOfCommas = len%3 == 0 ? nOfCommas-1 : nOfCommas;
+
+//         int count = n-1000;
+        
+//         return nOfCommas * (count+1);        
+//     }
+// }
