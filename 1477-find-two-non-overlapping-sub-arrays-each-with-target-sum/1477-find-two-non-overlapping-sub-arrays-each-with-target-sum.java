02ae25f910b25c6c3d9ad633187774a1ec5745d8
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-09-19 13:20:33 +0530

    Time: 7 ms (67.15%), Space: 90.9 MB (59.93%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,33 +1,13 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         int len = arr.length;
-        int[] prefix = new int[len];
         int[] suffix = new int[len];
-
-        Arrays.fill(prefix, Integer.MAX_VALUE);
         Arrays.fill(suffix, Integer.MAX_VALUE);
 
-        int left=0, sum=0;
-        for(int right=0; right<len; right++){
-            sum += arr[right];
-            if (right > 0) {
-                prefix[right] = prefix[right - 1];
-            }
-            if (sum == target) {
-                prefix[right] = Math.min(prefix[right], right - left + 1);
-            }
-            while(sum >= target){
-                if (sum == target) {
-                    prefix[right] = Math.min(prefix[right], right - left + 1);
-                }
-                sum -= arr[left];
-                left++;
-            }
-        }
+        int ans = Integer.MAX_VALUE;
+        int right = len - 1, sum = 0;
 
-        int right = len - 1; sum = 0;
-
-        for (left = len - 1; left >= 0; left--) {
+        for (int left = len - 1; left >= 0; left--) {
             sum += arr[left];
             if (left < len - 1) {
                 suffix[left] = suffix[left + 1];
@@ -37,17 +17,14 @@ class Solution {
             }
             while(sum >= target){
                 if (sum == target) {
-                    suffix[left] = Math.min(suffix[left], right - left + 1);
+                    int currentLen = right-left+1;
+                    if(right+1 < len && suffix[right+1] != Integer.MAX_VALUE){
+                        ans = Math.min(ans, currentLen + suffix[right+1]);
+                    }
+                    suffix[left] = Math.min(suffix[left], currentLen);
                 }
                 sum -= arr[right];
                 right--;
-            }
-        }
-
-        int ans = Integer.MAX_VALUE;
-        for(int i=0; i<len-1; i++){
-            if (prefix[i] != Integer.MAX_VALUE && suffix[i + 1] != Integer.MAX_VALUE) {
-                ans = Math.min(ans, prefix[i] + suffix[i + 1]);
             }
         }
 
