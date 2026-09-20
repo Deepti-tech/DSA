9a812d52cfe2c976bdf6e3b2e504eed3e0765d48
Deepti Singh <80256433+Deepti-tech@users.noreply.github.com>
2026-09-20 15:40:52 +0530

    Time: 8 ms (68.12%), Space: 46 MB (78.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,14 +4,20 @@ class Solution {
         int left = 0, maxFreq = 0, max = 0;
 
         for (int right = 0; right < s.length(); right++) {
+            
+            // Add new char in window
             freq[s.charAt(right) - 'A']++;
             maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+
+            // reduce the window if more new char are in the window than allowed
             int noOfUpdatedChar = right-left+1 - maxFreq;
             while (noOfUpdatedChar > k) {
                 freq[s.charAt(left) - 'A']--;
                 left++;
                 noOfUpdatedChar = right-left+1 - maxFreq;
             }
+
+            // check the max length of window
             max = Math.max(max, right-left+1);
         }
         return max;
